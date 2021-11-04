064fad91a79ea4ce8a02daf49e5e69f074c1bb02
Tiago Behenck <tiago.behenck.santos@gmail.com>
2021-11-04 17:48:00 -0300

    :nail_care: create Highlight component with TDD

---
+++ src/components/Highlight/stories.tsx
@@ -0,0 +1,16 @@
+import { Story, Meta } from '@storybook/react'
+import Highlight, { HighlightProps } from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  args: {
+    title: 'Read Dead it’s back',
+    subtitle: 'Come see John’s new adventures',
+    backgroundImage: '/img/red-dead-img.jpg',
+    buttonLabel: 'Buy now',
+    buttonLink: '/rdr2'
+  }
+} as Meta
+
+export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />
