25a7d5224a8f7ea1f477e53dbddfc20dca5ace41
Tiago Behenck <tiago.behenck.santos@gmail.com>
2021-11-04 18:15:51 -0300

    :nail_care: Add FlotImage into Highlight component

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -10,7 +10,24 @@ export default {
     backgroundImage: '/img/red-dead-img.jpg',
     buttonLabel: 'Buy now',
     buttonLink: '/rdr2'
+  },
+  parameters: {
+    layout: 'fullscreen'
   }
 } as Meta
 
-export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />
+export const Default: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+export const withFloatImage: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+withFloatImage.args = {
+  floatImage: '/img/red-dead-float.png'
+}
