8f55ef72c22ec630b0ddc8910a054ce7bf6a7192
Tiago Behenck <tiago.behenck.santos@gmail.com>
2021-11-22 21:38:14 -0300

    Create banner slider

--- src/components/BannerSlider/index.tsx
+++ src/components/BannerSlider/index.tsx
@@ -1,8 +1,36 @@
+import Banner, { BannerProps } from 'components/Banner'
+import Slider, { SliderSettings } from 'components/Slider'
+
 import * as S from './styles'
 
-const BannerSlider = () => (
+export type BannerSliderProps = {
+  items: BannerProps[]
+}
+
+const settings: SliderSettings = {
+  dots: true,
+  arrows: false,
+  vertical: true,
+  verticalSwiping: true,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        vertical: false,
+        verticalSwiping: false
+      }
+    }
+  ]
+}
+
+const BannerSlider = ({ items }: BannerSliderProps) => (
   <S.Wrapper>
-    <h1>BannerSlider</h1>
+    <Slider settings={settings}>
+      {items.map((item) => (
+        <Banner key={item.title} {...item} />
+      ))}
+    </Slider>
   </S.Wrapper>
 )
 
