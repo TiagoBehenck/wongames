30f227d3afa7c0c86a8f12f3f5471af977a75ca4
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2022-01-17 17:03:03 -0300

    generate text field

---
+++ src/components/TextField/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+import TextField from '.'
+
+export default {
+  title: 'TextField',
+  component: TextField
+} as Meta
+
+export const Default: Story = () => <TextField />
