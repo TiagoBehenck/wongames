018c5b0f28ce64a60ebcca0a6f5072414dc1410e
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2022-01-18 09:14:24 -0300

    create a Auth template with tests

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -1,13 +1,42 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Auth from '.'
 
 describe('<Auth />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Auth />)
+  it('should render all components and children', () => {
+    renderWithTheme(
+      <Auth title="Auth Title">
+        <input type="text" />
+      </Auth>
+    )
 
-    expect(screen.getByRole('heading', { name: /Auth/i })).toBeInTheDocument()
+    // verificar 2 logos
+    // verificar se tem o heading principal do banner OK
+    // verificar se tem subtitle OK
+    // verifica se tem o title do content OK
+    // verifica se o children esta sendo renderizado OK
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+
+    expect(
+      screen.getByRole('heading', {
+        name: /All your favorite games in one place/i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /won is the best and most complete gaming plataform./i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /Auth Title/i
+      })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
   })
 })
