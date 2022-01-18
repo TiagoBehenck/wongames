08ded8489ba32028f9c5b4f42779aa4adf58b33d
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2022-01-18 13:59:59 -0300

    create a component to form sign in

---
+++ src/components/FormSignIn/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+import FormSignIn from '.'
+
+export default {
+  title: 'Form/FormSignIn',
+  component: FormSignIn
+} as Meta
+
+export const Default: Story = () => <FormSignIn />
---
+++ src/components/FormSignIn/test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+
+import FormSignIn from '.'
+
+describe('<FormSignIn />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<FormSignIn />)
+
+    expect(
+      screen.getByRole('heading', { name: /FormSignIn/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
