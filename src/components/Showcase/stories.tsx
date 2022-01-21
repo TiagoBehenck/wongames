c67c25444022e5bc8a9ac8192ab93097bd06d05c
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2022-01-21 14:37:05 -0300

    component showcase

--- src/components/Showcase/stories.tsx
+++ src/components/Showcase/stories.tsx
@@ -1,9 +1,49 @@
 import { Story, Meta } from '@storybook/react'
-import Showcase from '.'
+import Showcase, { ShowcaseProps } from '.'
+
+import highlightMock from 'components/Highlight/mock'
+import gamesMock from 'components/GameCardSlider/mock'
 
 export default {
   title: 'Showcase',
-  component: Showcase
+  component: Showcase,
+  decorators: [
+    (Story) => (
+      <div style={{ margin: '0 auto' }}>
+        <Story />
+      </div>
+    )
+  ],
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
-export const Default: Story = () => <Showcase />
+export const Default: Story<ShowcaseProps> = (args) => <Showcase {...args} />
+
+Default.args = {
+  title: 'Most Popular',
+  highlight: highlightMock,
+  games: gamesMock
+}
+
+export const withoutHighlight: Story<ShowcaseProps> = (args) => (
+  <Showcase {...args} />
+)
+
+withoutHighlight.args = {
+  title: 'Most Popular',
+  games: gamesMock
+}
+
+export const withoutGames: Story<ShowcaseProps> = (args) => (
+  <Showcase {...args} />
+)
+
+withoutGames.args = {
+  title: 'Most Popular',
+  highlight: highlightMock
+}
