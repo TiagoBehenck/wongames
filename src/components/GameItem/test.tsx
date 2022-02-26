810efec188dd8ba9017b0624891a7be87d9905ad
Tiago Behenck <tiago.behenck.santos@gmail.com>
2022-02-26 14:44:00 -0300

    some stuffs

---
+++ src/components/GameItem/test.tsx
@@ -0,0 +1,56 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameItem, { GameItemProps } from '.'
+
+const props: GameItemProps = {
+  img: 'img',
+  title: 'title',
+  price: 'price'
+}
+
+describe('<GameItem />', () => {
+  it('should render the item', () => {
+    renderWithTheme(<GameItem {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+
+    expect(screen.getByText(props.price)).toBeInTheDocument()
+  })
+
+  it('should render the item with download link', () => {
+    const downloadLink = 'http://link'
+
+    renderWithTheme(<GameItem {...props} downloadLink={downloadLink} />)
+
+    expect(
+      screen.getByRole('link', { name: `Get ${props.title} here` })
+    ).toHaveAttribute('href', downloadLink)
+  })
+
+  it('should render the payment info', () => {
+    const paymentInfo = {
+      number: '**** **** **** 4326',
+      flag: 'mastercard',
+      img: '/img/master-card.png',
+      purchaseDate: 'Purchased mande on 07/20/2022 at 20:32'
+    }
+
+    renderWithTheme(<GameItem {...props} paymentInfo={paymentInfo} />)
+
+    expect(screen.getByRole('img', { name: paymentInfo.flag })).toHaveAttribute(
+      'src',
+      paymentInfo.img
+    )
+
+    expect(screen.getByText(paymentInfo.number)).toBeInTheDocument()
+    expect(screen.getByText(paymentInfo.purchaseDate)).toBeInTheDocument()
+  })
+})
