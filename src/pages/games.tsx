c1ed974ea720b7de2d4f69f27f013d3811f0c5ec
Tiago Behenck <tiago.behenck.santos@gmail.com>
2022-03-28 19:11:58 -0300

    add Studio Apollo

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,6 +1,8 @@
-import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
+import { initializeApollo } from 'utils/apollo'
 
-import gamesItemsMock from 'components/GameCardSlider/mock'
+import { QUERY_GAMES } from 'graphql/queries/games'
+
+import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 import filterItemsMock from 'components/ExploreSidebar/mock'
 
 export default function GamesPage(props: GamesTemplateProps) {
@@ -8,9 +10,25 @@ export default function GamesPage(props: GamesTemplateProps) {
 }
 
 export async function getServerSideProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query({
+    query: QUERY_GAMES,
+    variables: { limit: 9 }
+  })
+
   return {
     props: {
-      games: gamesItemsMock,
+      revalidate: 1,
+      games: data.games.map((game) => ({
+        title: game.name,
+        developer: game.developers[0].name,
+        img: game?.cover?.url || '',
+        price: new Intl.NumberFormat('en', {
+          style: 'currency',
+          currency: 'USD'
+        }).format(game.price)
+      })),
       filterItems: filterItemsMock
     }
   }
