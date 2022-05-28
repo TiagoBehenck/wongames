bce3ce29a32c6d386ee85d69832c63df8d8edd40
Tiago Behenck <tiago.behenck.santos@gmail.com>
2022-05-27 23:22:51 -0300

    create utils to parse the query string

---
+++ src/utils/filter/index.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ItemProps } from 'components/ExploreSidebar'
+import { ParsedUrlQueryInput } from 'querystring'
+
+type ParseArgs = {
+  queryString: ParsedUrlQueryInput
+  filterItems: Pick<ItemProps, 'type' | 'name'>[]
+}
+
+export const parseQueryStringToWhere = ({
+  queryString,
+  filterItems
+}: ParseArgs) => {
+  const obj: any = {}
+
+  Object.keys(queryString)
+    .filter((item) => item !== 'sort')
+    .map((key) => {
+      const item = filterItems?.find((item) => item.name === key)
+      const isCheckBox = item?.type === 'checkbox'
+
+      obj[key] = isCheckBox
+        ? { name_contains: queryString[key] }
+        : queryString[key]
+    })
+
+  return obj
+}
+
+export const parseQueryStringToFilter = ({
+  queryString,
+  filterItems
+}: ParseArgs) => {
+  const obj: any = {}
+
+  Object.keys(queryString).forEach((key) => {
+    const item = filterItems?.find((item) => item.name === key)
+    const isCheckBox = item?.type === 'checkbox'
+    const isArray = Array.isArray(queryString[key])
+
+    obj[key] = !isArray && isCheckBox ? [queryString[key]] : queryString[key]
+  })
+
+  return obj
+}
