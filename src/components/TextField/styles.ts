6c833c71d465ceb1a3895e0c05b9c2a3e20ea0ac
Tiago Behenck <tiago.behenck.santos@gmail.com>
2022-06-21 20:15:27 -0300

    adjust the default style into diferents browsers

--- src/components/TextField/styles.ts
+++ src/components/TextField/styles.ts
@@ -84,7 +84,13 @@ export const Input = styled.input<InputPositionProps>`
     background: transparent;
     border: 0;
     outline: none;
-    width: 100%;
+    width: ${iconPosition === 'right' ? `calc(100% - 2.2rem)` : `100%`};
+
+    &:-webkit-autofill {
+      -webkit-box-shadow: 0 0 0 ${theme.spacings.small}
+        ${theme.colors.lightGray} inset;
+      filter: none;
+    }
   `}
 `
 
