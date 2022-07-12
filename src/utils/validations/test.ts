c753bfd9c419b48b08cd2c3bb80156c86a7f0763
Tiago Behenck <tiago.behenck.santos@gmail.com>
2022-07-12 20:57:36 -0300

    add validation with Joi

---
+++ src/utils/validations/test.ts
@@ -0,0 +1,75 @@
+import { signUpValidate, signInValidate, SignInValues } from '.'
+
+describe('validations', () => {
+  describe('signInValidate()', () => {
+    it('should validate empty field', () => {
+      const values: SignInValues = {
+        email: '',
+        password: ''
+      }
+
+      expect(signInValidate(values)).toMatchObject({
+        email: '"email" is not allowed to be empty',
+        password: '"password" is not allowed to be empty'
+      })
+    })
+
+    it('should return invalid email error', () => {
+      const values = { email: 'invalid-email', password: '' }
+
+      expect(signInValidate(values).email).toMatchInlineSnapshot(
+        `"\\"email\\" must be a valid email"`
+      )
+    })
+  })
+
+  describe('signUpValidate()', () => {
+    it('should validate empty field', () => {
+      const values = {
+        username: '',
+        email: '',
+        password: ''
+      }
+
+      expect(signUpValidate(values)).toMatchObject({
+        email: expect.any(String),
+        username: expect.any(String),
+        password: expect.any(String),
+        confirm_password: expect.any(String)
+      })
+    })
+
+    it('should return short username error', () => {
+      const values = { username: 'hi', email: '', password: '' }
+
+      expect(signUpValidate(values).username).toMatchInlineSnapshot(
+        `"\\"username\\" length must be at least 5 characters long"`
+      )
+    })
+
+    it('should return invalid email error', () => {
+      const values = {
+        username: 'wongames',
+        email: 'invalid-email',
+        password: ''
+      }
+
+      expect(signUpValidate(values).email).toMatchInlineSnapshot(
+        `"\\"email\\" must be a valid email"`
+      )
+    })
+
+    it('should return error if password does not match with confirm password', () => {
+      const values = {
+        username: 'won',
+        email: 'won@won.com',
+        password: '1234',
+        confirm_password: '4321'
+      }
+
+      expect(signUpValidate(values).confirm_password).toMatchInlineSnapshot(
+        `"confirm password does not match with password"`
+      )
+    })
+  })
+})
